8d0d19fe2a4369e1d3f1870200247092324e1d8f
voenkom <pankovsemion@yandex.ru>
2024-08-29 22:09:41 +0500

    finish command

--- src/widget.py
+++ src/widget.py
@@ -5,4 +5,6 @@ def mask_account_card(card_number: str) -> str:
         return f'{card_number[:-16]}{card_number[:5]} {card_number[4:6]} {"*" * 2} {"*" * 4} {card_number[12:]}'
 
 
+def get_date(date: str) -> str:
+    return f"{date[8:9]}.{date[5:6]}.{date[3:]}"
 
