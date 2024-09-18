8b35050ae394e07517092cfe4d8b0bf124318ca1
voenkom <pankovsemion@yandex.ru>
2024-09-18 22:37:05 +0500

    init commit

--- src/masks.py
+++ src/masks.py
@@ -1,6 +1,8 @@
 def get_mask_card_number(card_number: str):
+    """Функция маскирует номер карты"""
     return f'{card_number[:5]} {card_number[4:6]} {"*" * 2} {"*" * 4} {card_number[12:]}'
 
 
 def get_mask_account(score_number: str):
+    """Функция маскирует счет"""
     return f'{"*" * 2} {score_number[16:]}'
--- src/widget.py
+++ src/widget.py
@@ -1,10 +1,21 @@
-def mask_account_card(card_number: str) -> str:
-    if 3 == "Счет":
-       return f'{card_number[:-20]}{card_number[:-16]}'
-    else:
-        return f'{card_number[:-16]}{card_number[:5]} {card_number[4:6]} {"*" * 2} {"*" * 4} {card_number[12:]}'
+from src.masks import get_mask_account,get_mask_card_number
 
+def mask_account_card(card_string: str) -> str | None:
+    """Функции маскируют счеты и карты"""
+    list_card_string = card_string.split()
+    if "Visa Platinum" in card_string or "Maestro" in card_string:
+        return f'{" ".join(list_card_string[:-1])} {get_mask_card_number(list_card_string[-1])}'
+    else:
+        return f'{"".join(list_card_string[:-1])} {get_mask_account(list_card_string[-1])}'
+    return None
+
+print(mask_account_card("Visa Platinum 7000792289606361"))
+print(mask_account_card("Счет 73654108430135874305"))
 
 def get_date(date: str) -> str:
-    return f"{date[8:9]}.{date[5:6]}.{date[3:]}"
+    """Функция меняет формат даты"""
+    return f"{date[8:10]}.{date[5:7]}.{date[:4]}"
+
+print(get_date("2024-03-11T02:26:18.671407"))
+
 
